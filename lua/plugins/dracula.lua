3e9ee05b9acbf051b07c9baabee3d0c53774a527
EthanGilles <ehgilles01@gmail.com>
2024-04-29 09:42:33 -0400

    Catpuccin theme

--- lua/plugins/dracula.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'dracula/vim',
-  lazy = false,
-  priority = 998,
-  config = function()
-    vim.cmd('colorscheme dracula')
-  end
-}
--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,9 +3,9 @@ local config = function()
     "RainbowRed",
     "RainbowYellow",
     "RainbowBlue",
+    "RainbowViolet",
     "RainbowOrange",
     "RainbowGreen",
-    "RainbowViolet",
     "RainbowCyan",
 }
 
@@ -13,12 +13,12 @@ local hooks = require "ibl.hooks"
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#FF79C6" })
     vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#8BE9FD" })
     vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
     vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#BD93F9" })
     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 

bf8016b2599b11596d42c624e04aca68a0f84dc9
EthanGilles <ehgilles01@gmail.com>
2024-04-30 22:52:22 -0400

    alpha

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,68 @@
+local config = function()
+  local alpha = require('alpha')
+  local dashboard = require('alpha.themes.dashboard')
+
+  dashboard.section.header.val = {
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                     ]],
+			[[       ████ ██████           █████      ██                     ]],
+			[[      ███████████             █████                             ]],
+			[[      █████████ ███████████████████ ███   ███████████   ]],
+			[[     █████████  ███    █████████████ █████ ██████████████   ]],
+			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+			[[                                                                       ]],
+			[[                               Ethan Gilles                            ]],
+			[[                                                                       ]],
+		}
+
+		_Gopts = {
+			position = "center",
+			hl = "Type",
+			-- wrap = "overflow";
+		}
+
+		-- Set menu
+		dashboard.section.buttons.val = {
+			-- dashboard.button("SPC j", "󰈚   Restore Session", ":SessionRestore<cr>"),
+			dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("f", "   Find file", ":cd $HOME | Telescope find_files<CR>"),
+			dashboard.button("g", "󰱼   Find word", ":Telescope live_grep<CR>"),
+			dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+			dashboard.button("l", "󰒲   Lazy", ":Lazy<CR>"),
+			dashboard.button("u", "󰂖   Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
+			dashboard.button("o", "󱌣   LSP Update", ":CocUpdate<CR>"),
+			dashboard.button("c", "   Config", ":e $MYVIMRC <CR>"),
+			dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
+		}
+    
+    local function footer()
+      local total_plugins = #require('lazy').plugins()
+      local datetime = os.date(" %d-%m-%Y   %H:%M:%S")
+      local version = vim.version()
+      local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
+
+      return datetime .. "   " .. total_plugins .. " plugins" .. nvim_version_info
+    end
+
+    dashboard.section.footer.val = footer()
+		dashboard.opts.opts.noautocmd = true
+		alpha.setup(dashboard.opts)
+
+		require("alpha").setup(dashboard.opts)
+
+end
+
+return {
+  'goolord/alpha-nvim',
+  lazy = false,
+  event = "VimEnter",
+  config = config,
+}

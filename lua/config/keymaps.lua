b09a83b540df501e87fad30a098bcdcbd768c9d1
EthanGilles <ehgilles01@gmail.com>
2024-05-22 11:09:07 -0400

    CoC keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -50,7 +50,8 @@ keymap.set("n", "<C-s>", require("auto-session.session-lens").search_session, op
 -- <SPACE+L+C> (Latex Clean) cleans the directory with latex files after compiling.
 keymap.set("n","<leader>lc", ":VimtexClean<CR>", {noremap = true})
 
--- Auto Completion --
+
+-- Auto Completion / CoC --
 function _G.check_back_space()
     local col = vim.fn.col('.') - 1
     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
@@ -69,3 +70,28 @@ keymap.set("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\
 keymap.set("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
 -- Use <c-space> to trigger completion
 keymap.set("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
+
+
+-- Use K to show documentation in preview window
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+
+-- Highlight the symbol and its references on a CursorHold event(cursor is idle)
+vim.api.nvim_create_augroup("CocGroup", {})
+vim.api.nvim_create_autocmd("CursorHold", {
+    group = "CocGroup",
+    command = "silent call CocActionAsync('highlight')",
+    desc = "Highlight symbol under cursor on CursorHold"
+})
+
+-- Symbol renaming
+keymap.set("n", "<leader>rn", "<Plug>(coc-rename)", {silent = true})

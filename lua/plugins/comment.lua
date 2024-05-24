b5639bd044125128a4717f0ce34dba6f22822801
EthanGilles <ehgilles01@gmail.com>
2024-05-24 14:59:43 -0400

    fix comment keybinds

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,15 @@
 local config = function ()
   local api = require('Comment.api')
+  local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
+
+  -- Toggle current line
   vim.keymap.set('n', '<C-_>', api.toggle.linewise.current, opts)
-  vim.keymap.set('v', '<C-_>', api.toggle.linewise.current, opts)
+
+  -- Toggle highlighted selection (linewise)
+  vim.keymap.set('x', '<C-_>', function()
+    vim.api.nvim_feedkeys(esc, 'nx', false)
+    api.toggle.linewise(vim.fn.visualmode())
+  end)
 end
 
 return {

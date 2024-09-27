5f64c9cdf9e469929b96416f27b59d9ae97083b3
ethangilles <ehgilles01@gmail.com>
2024-09-26 22:33:13 -0400

    hl chunk

---
+++ lua/plugins/hlchunk.lua
@@ -0,0 +1,36 @@
+return {
+  "shellRaining/hlchunk.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("hlchunk").setup({
+      chunk = {
+          enable = true,
+          priority = 15,
+      style = {
+          { fg = "#737994" },
+          { fg = "#c21f30" },
+      },
+      use_treesitter = true,
+      chars = {
+          horizontal_line = "─",
+          vertical_line = "│",
+          left_top = "┌",
+          left_bottom = "└",
+          right_arrow = "─",
+      },
+      textobject = "",
+      max_file_size = 1024 * 1024,
+      error_sign = true,
+      -- animation related
+      duration = 300,
+      delay = 120,
+      },
+      line_num = {
+        enable = true,
+        style = "#89b4fa",
+        priority = 10,
+        use_treesitter = false,
+      },
+    })
+  end
+}
